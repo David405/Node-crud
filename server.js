61d2169660a81450952ceea233884e9a2c02d70d
david405 <davidasamonye@gmail.com>
2019-07-05 15:09:39 +0100

    further setup

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ path = require('path'),
 bodyParser = require('body-parser'),
 cors = require('cors'),
 mongoose = require('mongoose');
+config = require('./DB');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+    () => {console.log('Database is connected')},
+    err => {console.log('Cannot connect to database'+ err)}
+);
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 let port = process.env.PORT || 4000;
 
 const server = app.listen(function(){
